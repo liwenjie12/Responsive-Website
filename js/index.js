71612ae2bea597245401a4ce0fcde484d83ba8b6
liwenjie12 <1499967138@qq.com>
2019-04-10 02:33:40 +0800

    .

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// window.onload=function () {
+//     var dom=document.getElementById("phone");
+//     var width=parseInt(window.getComputedStyle(dom).width);
+//     var height=parseInt(window.getComputedStyle(dom).height);
+//     console.log(height);
+//     console.log(width);
+//     width=width+height;
+//     console.log(width);
+//     height=width-height;
+//     console.log(height);
+//     width=width-height;
+//     dom.style.width=width+"px";
+//     dom.style.height=height+"px";
+//     dom.parentNode.style.height=width+"px";
+// }
