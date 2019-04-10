027e0492fd9f8e2b12c603b460904150ee4a2f7c
liwenjie12 <1499967138@qq.com>
2019-04-11 00:00:53 +0800

    设置导航响应

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
-// window.onload=function () {
-//     var dom=document.getElementById("phone");
-//     var width=parseInt(window.getComputedStyle(dom).width);
-//     var height=parseInt(window.getComputedStyle(dom).height);
-//     console.log(height);
-//     console.log(width);
-//     width=width+height;
-//     console.log(width);
-//     height=width-height;
-//     console.log(height);
-//     width=width-height;
-//     dom.style.width=width+"px";
-//     dom.style.height=height+"px";
-//     dom.parentNode.style.height=width+"px";
-// }
+window.onload=function () {
+   function check() {
+       var menu=document.getElementById("menu");
+       var nav=document.getElementsByTagName("nav")[0];
+       menu.onclick=function () {
+           switch (nav.className) {
+               case "":nav.className="scaleIn";break;
+               case "scaleIn":nav.className="scaleOut";break;
+               case "scaleOut":nav.className="scaleIn";break;
+           }
+       }
+   }
+   check();
+}
